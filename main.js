361e9e0172e1a5ba0980527cb7a85b54bfb53432
jaehyeon99 <janhyun99@naver.com>
2020-10-23 22:54:39 +0900

    add mobile view

---
+++ main.js
@@ -0,0 +1,7 @@
+function openNav(){
+    document.getElementById("close_nav").style.display="block";
+    document.getElementById("menu_bar").style.display ="block";
+}
+function CloseNav(){
+    document.getElementById("menu_bar").style.display="none";
+}
